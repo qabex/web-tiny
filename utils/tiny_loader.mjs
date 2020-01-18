20b08880a71c4979f6ca040c448c1ad6f729c313
Shane Holloway <shane.holloway@ieee.org>
2020-01-18 15:18:26 -0700

    Added tiny_loader implementation

---
+++ utils/tiny_loader.mjs
@@ -0,0 +1,23 @@
+export default function tiny_loader(opt) {
+  const D = document
+  const src = 'string' === typeof opt ? opt : opt.src
+  if (src === opt) opt = {src: opt}
+  if (src != encodeURI(src))
+    throw new Error('Invalid script src')
+
+  let s = D.querySelector('script[src="'+ src + '"]')
+  if (!s) {
+    s = D.createElement('script')
+
+    if (src.startsWith('http') && undefined === opt.crossorigin)
+      opt.crossorigin = 'anonymous'
+
+    D.head.appendChild(Object.assign(s, opt))
+  } else if (!s.promise)
+    return s.promise
+
+  return s.promise = new Promise((resolve, reject) => {
+    s.onload = e => resolve(e.target)
+    s.onerror = e => reject(e)
+  })
+}
